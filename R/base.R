1fb560d15d9cb986d7bdd4a4ff74c40dd62e412f
lubianat <tiago.lubiana.alves@usp.br>
2020-10-11 11:51:41 -0300

    code: Style with use_tidy_style

--- R/base.R
+++ R/base.R
@@ -6,21 +6,21 @@ NULL
 #' @param pmids A character vector of PMIDs
 #' @param density_alpha A density_alpha to modulate the weight of pages
 #' @export
-run_multiple_osmium <- function(pmids, density_alpha = 1){
-
-  list_of_results = list()
-  for (pmid in pmids){
+run_multiple_osmium <- function(pmids, density_alpha = 1) {
+  list_of_results <- list()
+  for (pmid in pmids) {
     print(pmid)
     tryCatch(
       expr = {
-    df <-run_single_osmium(pmid,density_alpha )
-    list_of_results[[pmid]] <- df },
+        df <- run_single_osmium(pmid, density_alpha)
+        list_of_results[[pmid]] <- df
+      },
       error = function(e) {
         sprintf("Failed for %s", pmid)
-
-  })
+      }
+    )
   }
-  result = bind_rows(list_of_results)
+  result <- bind_rows(list_of_results)
   return(result)
 }
 
@@ -41,25 +41,26 @@ run_single_osmium <- function(pmid, density_alpha) {
   pages <- calculate_pages(a$basic$pageInfo)
 
   osmium_score <- calculate_density(pages,
-                              citations,
-                              alpha = density_alpha)
+    citations,
+    alpha = density_alpha
+  )
   osmium <- round(osmium_score, 3)
   alpha <- density_alpha
   date <- Sys.Date()
 
   result <- data.frame(pmid, title, doi, citations,
-                       pages, osmium, alpha, date,
-                       stringsAsFactors = FALSE)
+    pages, osmium, alpha, date,
+    stringsAsFactors = FALSE
+  )
 
   return(result)
 }
 
-calculate_pages <- function(page_string){
-
+calculate_pages <- function(page_string) {
   tryCatch(
-    expr =  {
-      diff <- eval(parse(text=page_string))
-      n_pages = abs(diff) + 1
+    expr = {
+      diff <- eval(parse(text = page_string))
+      n_pages <- abs(diff) + 1
       return(n_pages)
     },
     error = function(e) {
@@ -67,17 +68,13 @@ calculate_pages <- function(page_string){
       return(n_pages)
     }
   )
-
-
 }
 
 
-calculate_density <- function(n_pages, n_citations, alpha){
-    if (is.na(n_pages)){
-      return(NA)
-    }
-    density = n_citations / (n_pages ** alpha)
-    return(density)
-
+calculate_density <- function(n_pages, n_citations, alpha) {
+  if (is.na(n_pages)) {
+    return(NA)
+  }
+  density <- n_citations / (n_pages**alpha)
+  return(density)
 }
-
--- tests/testthat/test-base.R
+++ tests/testthat/test-base.R
@@ -3,7 +3,6 @@ test_that("multiplication works", {
 })
 
 test_that("run_multiple_osmium runs", {
-
   pmids <- c("20123131", "15892874", "notanid")
   df <- run_multiple_osmium(pmids, density_alpha = 1)
 
@@ -28,56 +27,56 @@ test_that("run_multiple_osmium runs", {
 
   # The number of citations for this article
   # Might increase in the future
-  current_density <- 16/5 - 0.00001
+  current_density <- 16 / 5 - 0.00001
   expect_gt(
     df$osmium[1],
     current_density
   )
-
 })
 
 
 
 test_that("run_single_osmium runs", {
-
   pmids <- c("31322498")
   df <- run_single_osmium(pmids, density_alpha = 1)
-
 })
 
 test_that("calculate_pages work", {
-
   result <- calculate_pages("75 - 79")
-  expected = 5
+  expected <- 5
   expect_equal(result, expected)
-
 })
 
 test_that("calculate_density work", {
+  result <- calculate_density(
+    n_pages = 5,
+    n_citations = 100,
+    alpha = 1
+  )
+  expected <- 20
+  expect_equal(result, expected)
 
-    result <- calculate_density(n_pages = 5,
-                                n_citations = 100,
-                                alpha = 1)
-    expected = 20
-    expect_equal(result, expected)
+  result <- calculate_density(
+    n_pages = 5,
+    n_citations = 100,
+    alpha = 2
+  )
+  expected <- 4
+  expect_equal(result, expected)
 
-    result <- calculate_density(n_pages = 5,
-                                n_citations = 100,
-                                alpha = 2)
-    expected = 4
-    expect_equal(result, expected)
+  result <- calculate_density(
+    n_pages = 5,
+    n_citations = 100,
+    alpha = 3
+  )
+  expected <- 0.8
+  expect_equal(result, expected)
 
-    result <- calculate_density(n_pages = 5,
-                                n_citations = 100,
-                                alpha = 3)
-    expected = 0.8
-    expect_equal(result, expected)
-
-    result <- calculate_density(n_pages = NA,
-                                n_citations = 100,
-                                alpha = 1)
-    expected = NA
-    expect_equal(result, expected)
-
-  })
-
+  result <- calculate_density(
+    n_pages = NA,
+    n_citations = 100,
+    alpha = 1
+  )
+  expected <- NA
+  expect_equal(result, expected)
+})
